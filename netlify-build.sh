0a7f5419ea33966b9ee5a739307f3fe4cf7ff61a
stoand <stoand@protonmail.com>
2020-08-17 18:14:26 +0300

    netlify: fix missing cd

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git clone https://github.com/frangio/netlify-latex && \
 cp paper/guessrandom.tex netlify-latex/main.tex && \
-cd netlify-latex && bash ./build.sh main.tex && \
+(cd netlify-latex && bash ./build.sh main.tex) && \
 mkdir -p build_out && \
-cp main.pdf build_out/thesis-guessrandom.pdf && \
+cp netlify-latex/main.pdf build_out/thesis-guessrandom.pdf && \
 echo "<meta http-equiv=\"Refresh\" content=\"0; url='thesis-guessrandom.pdf'\" />" > build_out/index.html
 
 

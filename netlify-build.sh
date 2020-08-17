29bcec97b946591701b37e4be4509c40dc73a689
stoand <stoand@protonmail.com>
2020-08-17 18:11:08 +0300

    netlify: add build_out dir back in

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 git clone https://github.com/frangio/netlify-latex && \
 cp paper/guessrandom.tex netlify-latex/main.tex && \
-cd netlify-latex && bash ./build.sh main.tex
-
-# mkdir -p build_out && \
-# echo "pdf build failed" > build_out/index.html && \
-# sudo apt install texlive-latex-recommended texlive-pictures texlive-latex-extra texlive-fonts-extra && \
-# cd paper && \
-# pdflatex guessrandom.tex && \
-# cp guessrandom.pdf ../build_out && \
-# cd ../build_out &&
-# echo "<meta http-equiv=\"Refresh\" content=\"0; url='guessrandom.pdf'\" />" > index.html
+cd netlify-latex && bash ./build.sh main.tex && \
+mkdir -p build_out && \
+cp main.pdf build_out/thesis-guessrandom.pdf && \
+echo "<meta http-equiv=\"Refresh\" content=\"0; url='thesis-guessrandom.pdf'\" />" > build_out/index.html
 
 

7dc196524beb0f3df6f8c766eeb8fff2cdef9d8b
stoand <stoand@protonmail.com>
2020-08-17 18:03:06 +0300

    netlify: use latex repo

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-mkdir -p build_out && \
-echo "pdf build failed" > build_out/index.html && \
-sudo apt install texlive-latex-recommended texlive-pictures texlive-latex-extra texlive-fonts-extra && \
-cd paper && \
-pdflatex guessrandom.tex && \
-cp guessrandom.pdf ../build_out && \
-cd ../build_out &&
-echo "<meta http-equiv=\"Refresh\" content=\"0; url='guessrandom.pdf'\" />" > index.html
+git clone https://github.com/frangio/netlify-latex && \
+cp paper/guessrandom.tex netlify-latex/main.tex && \
+cd netlify-latex && ./build.sh
+
+# mkdir -p build_out && \
+# echo "pdf build failed" > build_out/index.html && \
+# sudo apt install texlive-latex-recommended texlive-pictures texlive-latex-extra texlive-fonts-extra && \
+# cd paper && \
+# pdflatex guessrandom.tex && \
+# cp guessrandom.pdf ../build_out && \
+# cd ../build_out &&
+# echo "<meta http-equiv=\"Refresh\" content=\"0; url='guessrandom.pdf'\" />" > index.html
 
 

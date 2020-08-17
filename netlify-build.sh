bca582af42f9c9bd71ddb74f059e7bd0c8361e23
stoand <stoand@protonmail.com>
2020-08-17 17:52:28 +0300

    add netlify build file

---
+++ netlify-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p build_out && \
+echo "pdf build failed" > build_out/index.html && \
+apt install texlive-latex-recommended texlive-pictures texlive-latex-extra texlive-fonts-extra && \
+cd paper && \
+pdflatex guessrandom.tex && \
+cp guessrandom.pdf ../build_out && \
+cd ../build_out &&
+echo "<meta http-equiv=\"Refresh\" content=\"0; url='guessrandom.pdf'\" />" > index.html
+
+

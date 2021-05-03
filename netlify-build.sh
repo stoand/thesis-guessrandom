47aea0b2d47dc5d786acab47144cd6502f91b1b6
stoand <stoand@protonmail.com>
2021-05-03 13:45:48 +0200

    netlify build : update netlify-latext version

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ([ -d netlify-latex ] || git clone https://github.com/frangio/netlify-latex) && \
-(cd netlify-latex && git checkout e62e3ca141cf3df8743b48018b66565abd4f782e) && \
+(cd netlify-latex && git checkout 022bb08730809003db5bc5a810a9a1fee0770b6d) && \
 cp paper/guessrandom.tex netlify-latex/main.tex && \
 (cd netlify-latex && bash ./build.sh main.tex) && \
 mkdir -p build_out && \

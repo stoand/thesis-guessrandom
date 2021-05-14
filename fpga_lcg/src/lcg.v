80ff4177f980d97d22bc568b42ff24212861b9bf
stoand <stoand@protonmail.com>
2021-05-14 11:56:52 +0200

    fpga_lcg: can generate first three values

--- fpga_lcg/src/lcg.v
+++ fpga_lcg/src/lcg.v
@@ -5,14 +5,17 @@ module lcg(
     input [31:0] MULTIPLIER, // a
     input [31:0] INCREMENT, // c
     
-    input [31:0] value, // X
+    input [31:0] seed, // X
     
-    output [31:0] next_value,
+    output [31:0] v0,
+    output [31:0] v1,
+    output [31:0] v2,
 );
     
-    
     always @(posedge CLK) begin
-        next_value = ((value * MULTIPLIER) + INCREMENT) % MODULUS;
+        v0 = ((seed * MULTIPLIER) + INCREMENT) % MODULUS;
+        v1 = ((v0 * MULTIPLIER) + INCREMENT) % MODULUS;
+        v2 = ((v1 * MULTIPLIER) + INCREMENT) % MODULUS;
     end
 endmodule
 
@@ -20,12 +23,11 @@ module testbench(input CLK);
     
     reg [31:0] counter = 0;
     
-    // initial seed
-    reg [31:0] value;
-    reg [31:0] next_value;
+    reg [31:0] seed;
     
-    initial 
-        value = 96;
+    reg [31:0] v0;
+    reg [31:0] v1;
+    reg [31:0] v2;
     
     lcg lcg0(
         .CLK(CLK),
@@ -34,21 +36,21 @@ module testbench(input CLK);
         .MULTIPLIER(4001),
         .INCREMENT(60211),
         
-        .value(value),
-        .next_value(next_value),
+        .seed(seed),
+        .v0(v0),
+        .v1(v1),
+        .v2(v2),
     );
     
     always @(posedge CLK) begin
         
-        if(counter == 1) begin
-            assert (next_value == 444307); 
-        end
-        
         if(counter == 2) begin
-            // assert (next_value == 466569); 
+            assert (v0 == 444307); 
+            assert (v1 == 466569); 
+            assert (v2 == 127141); 
         end
         
+        seed = 96;
         counter = counter + 1;
-        value = next_value;
     end
 endmodule

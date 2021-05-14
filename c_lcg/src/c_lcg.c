81dadadb896ba60c76111e50f53edc6362290188
stoand <stoand@protonmail.com>
2021-05-14 11:58:56 +0200

    c_lcg: remove "unsigned" from ints

--- c_lcg/src/c_lcg.c
+++ c_lcg/src/c_lcg.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-unsigned int MODULUS = 993441; // m
-unsigned int MULTIPLIER = 4001; // a
-unsigned int INCREMENT = 60211; // c
+int MODULUS = 993441; // m
+int MULTIPLIER = 4001; // a
+int INCREMENT = 60211; // c
 
-unsigned int gen_rand(unsigned int prev) {
+int gen_rand(int prev) {
     return ((prev * MULTIPLIER) + INCREMENT) % MODULUS;
 }
 
 int main() {
 
-  unsigned int seed = 96; // aka the starting value or X0
+  int seed = 96; // aka the starting value or X0
 
-  unsigned int out0 = gen_rand(seed);
-  unsigned int out1 = gen_rand(out0);
-  unsigned int out2 = gen_rand(out1);
+  int out0 = gen_rand(seed);
+  int out1 = gen_rand(out0);
+  int out2 = gen_rand(out1);
 
   printf("first outputs for seed: %i \n%i\n%i\n%i\n", seed, out0, out1, out2);
 }

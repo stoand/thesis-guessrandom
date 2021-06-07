dd56527cd2598ba374abff31dcf0e9e010f72209
stoand <stoand@protonmail.com>
2021-06-07 11:17:16 +0200

    present: build presentation with netlify

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [View Paper as PDF](https://thesis-guessrandom.netlify.app/thesis-guessrandom.pdf)
 
+[View Presentation as PDF](https://thesis-guessrandom.netlify.app/thesis-guessrandom-presentation.pdf)
+
 My bachelor thesis - guessing mt_rand internal state in order to predict future random numbers
 
 See `./design` directory for more information on individual components.
--- netlify-build.sh
+++ netlify-build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+mkdir -p build_out && \
+
+# Setup build script
 ([ -d netlify-latex ] || git clone https://github.com/frangio/netlify-latex) && \
 (cd netlify-latex && git checkout 022bb08730809003db5bc5a810a9a1fee0770b6d) && \
+
+# Build paper
 cp paper/guessrandom.tex netlify-latex/main.tex && \
 (cd netlify-latex && bash ./build.sh main.tex) && \
-mkdir -p build_out && \
 cp netlify-latex/main.pdf build_out/thesis-guessrandom.pdf && \
+
+# Build presentation
+cp present/present.tex netlify-latex/main.tex && \
+(cd netlify-latex && bash ./build.sh main.tex) && \
+cp netlify-latex/main.pdf build_out/thesis-guessrandom-presentation.pdf && \
+
 echo "<meta http-equiv=\"Refresh\" content=\"0; url='thesis-guessrandom.pdf'\" />" > build_out/index.html
+
 
 
